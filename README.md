ee6c894feaa9052d57e513f1f703dca7682327e8
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2025-05-30 13:29:37 +0900

    Add error message

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # dateRn-rs
 Only Rename any filename to YYYY-MM-DD
+## Usage
+`dateRn "the path of target file"`  
+If your target file is "hoge.txt", the filename will be YYYY-MM-DD.txt
 ## Build & Run
 Use built file from [releases page](https://github.com/yy-tromb/dateRn-rs/releases).  
 For self-build, clone this repository, enter directory, and use [Cargo](https://doc.rust-lang.org/cargo/index.html) (by execute `cargo build --release`).
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use std::error::Error;
 use chrono::Utc;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let target_path = std::env::args().nth(1).unwrap();
+    let target_path = std::env::args().nth(1).expect("\
+        \x1b[1;4;31mMaybe no arguments.\x1b[0;1;4m\n\
+        Use `dateRn \"the path of target file\"`\x1b[0m\
+    ");
     let target_path = std::path::PathBuf::from(target_path);
     let today = Utc::now().format("%Y-%m-%d").to_string();
     let mut updated_path = target_path.clone();
