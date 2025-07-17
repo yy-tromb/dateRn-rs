fd5b2d20993581a56f439abdf88dbc84933220b0
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2025-07-17 11:57:04 +0900

    Fix renamed name today->created day

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use chrono::{DateTime, Local};
 use std::error::Error;
-use chrono::Local;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let target_path = std::env::args().nth(1).expect("\
+    let target_path = std::env::args().nth(1).expect(
+        "\
         \x1b[1;4;31mMaybe no arguments.\x1b[0;1;4m\n\
         Use `dateRn \"the path of target file\"`\x1b[0m\
-    ");
+    ",
+    );
     let target_path = std::path::PathBuf::from(target_path);
-    let today = Local::now().format("%Y-%m-%d").to_string();
+    let created_time = std::fs::metadata(&target_path).unwrap().created().unwrap();
+    let created_date: DateTime<Local> = created_time.into();
+    let created_date = created_date.format("%Y-%m-%d").to_string();
     let mut updated_path = target_path.clone();
-    updated_path.set_file_name(today);
+    updated_path.set_file_name(created_date);
     if let Some(extension) = target_path.extension() {
         updated_path.set_extension(extension);
     }
-    std::fs::rename(target_path,updated_path).unwrap();
+    std::fs::rename(target_path, updated_path).unwrap();
     Ok(())
 }
